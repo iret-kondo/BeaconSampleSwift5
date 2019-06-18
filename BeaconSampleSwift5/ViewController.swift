ba6b9403b29f0efd36d4472638d50db9331f0484
近藤 寛志 <kondo@cloudpack.jp>
2019-06-18 18:17:25 +0900

    initial commit.

---
+++ BeaconSampleSwift5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeaconSampleSwift5
+//
+//  Created by 近藤 寛志 on 2019/06/18.
+//  Copyright © 2019 iret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
